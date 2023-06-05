e058bc6e0bba4ed755ffec213719fc1a1227e7a6
Rainier Silva <rainiersilva14@gmail.com>
2023-06-04 22:45:06 -0400

    primera entrega

--- Documentos/Proyectos JS/Primera_Entrega/conversor.js
+++ Documentos/Proyectos JS/Primera_Entrega/conversor.js
@@ -1,5 +1,5 @@
 // Conversor de moneda
-//Tasas de conversión
+// Tasas de conversión
 const tasaPesosEuro = 854.51;
 const tasaPesosDolar = 798.94;
 
@@ -13,6 +13,7 @@ let continuar = true;
 while (continuar) {
   // Solicitar al usuario la cantidad y la moneda de origen
   let cantidad = parseFloat(prompt('Ingrese la cantidad a convertir:'));
+
   let monedaOrigen = prompt(
     'Ingrese la moneda de origen (USD o EUR):'
   ).toUpperCase();
@@ -27,7 +28,7 @@ while (continuar) {
     resultado = 'Moneda de origen no válida.';
   }
 
-  // Mostrar el resultado de la conversión
+  // Mostrar el resultado
   alert(`Resultado de la conversión: ${resultado.toFixed(2)} $`);
 
   // Preguntar al usuario si desea realizar otra conversión
